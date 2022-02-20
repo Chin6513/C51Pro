47edefabbf11f677dbd3782708becdf1530682b1
秦 <Chin6513@163.com>
2022-02-20 21:45:26 +0800

    分模块
    修改了编码格式，尝试分模块完成一个项目

---
+++ delay.c
@@ -0,0 +1,6 @@
+﻿void delay(unsigned int xms)
+{
+	unsigned int i;
+	for(;xms>0;xms--)
+	for(i=110;i>0;i--);
+}
---
+++ delay.h
@@ -0,0 +1,6 @@
+﻿#ifndef __DELAY_H__
+#define __DELAY_H__
+
+void delay(unsigned int xms);
+
+#endif
